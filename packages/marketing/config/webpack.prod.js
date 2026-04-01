87ee69c8bb37b167dd72d4bcfb7b412aa184bc53
rkeerthick <rkeerthick1@gmail.com>
2026-04-01 19:36:45 +0530

    Container workflow added

---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.common');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json');
+
+const prodConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js',
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './MarketingApp': './src/bootstrap.js',
+            },
+            shared: packageJson.dependencies,
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, prodConfig);
