b2c303733020b033412b6c43650a86bf272dbcd3
rkeerthick <rkeerthick1@gmail.com>
2026-04-06 19:24:42 +0530

    Custom styles ID added

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,22 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from "./components/Header";
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+});
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Header />
-            <Switch>
-                <Route path="/" component={MarketingApp}  />
-            </Switch>
-        </BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <Header />
+                <Switch>
+                    <Route path="/" component={MarketingApp}  />
+                </Switch>
+            </BrowserRouter>
+        </StylesProvider>
     );
 };
 
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma',
+});
+
 const App = ({ history }) => {
     return (
-        <StylesProvider>
+        <StylesProvider generateClassName={generateClassName}>
             <Router history={history}>
                 <Switch>
                     <Route path="/pricing" component={Pricing} />
